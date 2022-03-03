1d01f67c77ed3d5e03a29c4a27907ddad1f5c2cc
Laura Gonzalez Martin <Lau@Pro.local>
2022-03-03 19:44:45 -0300

    commit nro 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tabla-cli :titulo="titulo[0]" :miarray="listaSoporte" :misestilos="estilosSoporte" :miencabezado="encabezado"/>
+    <tabla-cli :titulo="titulo[1]" :miarray="listaDirectivos" :misestilos="estilosDirectivos" :miencabezado="encabezado"/>
+    <tabla-cli :titulo="titulo[2]" :miarray="listaIt" :misestilos="estilosIt" :miencabezado="encabezado"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TablaCli from './components/TablaCli.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TablaCli
+  },
+  data(){
+    return{
+      titulo:["Usuarios de Soporte Tecnico","Usuarios del Area de Gerencia","Usuarios de Sector It"],
+          listaSoporte:[
+              {usuario:"@lucc__34",clave:"45Ad45"},
+              {usuario:"@starwars10",clave:"St4rW4rs"},
+              {usuario:"@Jimenez99",clave:"yo8787*"}
+          ],
+          listaDirectivos:[
+              {usuario:"@TorresJuanManuel",clave:"**9090A"},
+              {usuario:"@PerezAlberto",clave:"P*12345"},
+              {usuario:"@GonzalezHugo",clave:"yo8787*"}
+          ],
+          listaIt:[
+              {usuario:"@linux2022",clave:"L1nux2022_"},
+              {usuario:"@SteveJobs",clave:"mac9876"},
+              {usuario:"@ProgrammerX",clave:"101011Binary"}
+          ],
+          estilosSoporte:"table table-striped",
+          estilosDirectivos:"table table-bordered text-danger",
+          estilosIt:"table table-dark",
+          encabezado:true
+
+    }
+          
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TablaCli.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="row">
+        <h4>{{titulo}}</h4>
+        <article class="col-md-4">
+            <table :class="misestilos">
+                <thead v-if="miencabezado">
+                    <tr>
+                        <th>Usuario</th>
+                        <th>Clave</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item,index) of miarray" :key="index" >         
+                        <td>{{item.usuario}}</td> 
+                        <td>{{item.clave}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </article>
+    </section>
+</template>
+
+<script>
+    export default ({
+        props:{
+            titulo:String,
+            miarray: Array,
+            misestilos: String,
+            miencabezado: Boolean
+        }
+    })
+</script>
