11f2e0121f79fabac9b6e850403a4fc29af66250
Laura Gonzalez Martin <Lau@Pro.local>
2022-03-09 10:03:09 -0300

    tabulaciones corregidas

--- src/App.vue
+++ src/App.vue
@@ -17,25 +17,25 @@ export default {
   data(){
     return{
       titulo:["Usuarios de Soporte Tecnico","Usuarios del Area Gerencial","Usuarios de Sector It"],
-          listaSoporte:[
-              {usuario:"@lucc__34",clave:"45Ad45"},
-              {usuario:"@starwars1980",clave:"St4rW4rs"},
-              {usuario:"@Jimenez99",clave:"yo8787*"}
-          ],
-          listaDirectivos:[
-              {usuario:"@TorresJuanManuel",clave:"**9090A"},
-              {usuario:"@PerezAlberto",clave:"P*12345"},
-              {usuario:"@GonzalezHugo",clave:"yo8787*"}
-          ],
-          listaIt:[
-              {usuario:"@linux2022",clave:"L1nux2022_"},
-              {usuario:"@SteveJobs",clave:"mac9876"},
-              {usuario:"@ProgrammerX",clave:"101011Binary"}
-          ],
-          estilosSoporte:"table table-striped",
-          estilosDirectivos:"table table-bordered text-danger",
-          estilosIt:"table table-dark",
-          encabezado:true
+      listaSoporte:[
+          {usuario:"@lucc__34",clave:"45Ad45"},
+          {usuario:"@starwars1980",clave:"St4rW4rs"},
+          {usuario:"@Jimenez99",clave:"yo8787*"}
+      ],
+      listaDirectivos:[
+          {usuario:"@TorresJuanManuel",clave:"**9090A"},
+          {usuario:"@PerezAlberto",clave:"P*12345"},
+          {usuario:"@GonzalezHugo",clave:"yo8787*"}
+      ],
+      listaIt:[
+          {usuario:"@linux2022",clave:"L1nux2022_"},
+          {usuario:"@SteveJobs",clave:"mac9876"},
+          {usuario:"@ProgrammerX",clave:"101011Binary"}
+      ],
+      estilosSoporte:"table table-striped",
+      estilosDirectivos:"table table-bordered text-danger",
+      estilosIt:"table table-dark",
+      encabezado:true
 
     }
           
